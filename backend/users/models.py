b8ff223b426b3e0e22c7ed4873610d54fbfe6002
pemtshewang <pemtshewang500@gmail.com>
2022-10-05 11:32:45 +0600

    10/05/2022

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,9 +2,9 @@ import email
 from email.policy import default
 from enum import unique
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser 
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
 
-class Alumni(AbstractBaseUser):
+class Alumni(AbstractBaseUser, PermissionsMixin):
 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -12,13 +12,14 @@ class Alumni(AbstractBaseUser):
     email = models.CharField(max_length=200, unique=True)
     password = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=10, unique=True)
-    is_staff = False
-    is_active = True
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    # objects = 
 
     USER_NAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name","phone_number"]
 
-    # objects = 
 
     def __str__(self) -> str:
         return self.email
